c486a6db25eea0a829649d95a8ba4e570e0b9cda
Evgenyi <evgenyisavizki@gmail.com>
2025-01-26 22:23:45 +0300

    load new model

--- constants.js
+++ constants.js
@@ -1,11 +1,26 @@
-export const leftWall = 'LeftWall';
-export const rightWall = 'RightWall';
-export const frontWall = 'FrontWall';
-export const backWall = 'BackWall';
-export const floor = 'Floor';
-export const door = 'Door';
-export const frontMetalicWall = 'FrontMetalicWall';
-export const scoreboard = 'Scoreboard';
-export const frontWallHandrail = 'HandRail1';
-export const rightWallHandrail = 'HandRail2';
-export const preScoreBoard = 'PreScoreBoard';
+export const elements = {
+    Ceiling:['ceiling1','ceiling2'],
+    Lamp:['Lamp1','Lamp2'],
+    Door:['RightDoor','LeftDoor'],
+    Door1:['RightDoor1','LeftDoor1'],
+    ControlPanel:['ControlPanel1','ControlPanel2','ControlPanel3'],
+    FrontWall:['FrontWall1','FrontWall2','FrontWall3','FrontWall4'],
+    BackWall:['BackWall1','BackWall2','BackWall3','BackWall4'],
+    BackWall1:['BackWall11','BackWall12','BackWall13','BackWall14'],
+    BackHandrail:['BackHandrail11','BackHandrail12','BackHandrail13'],
+    RightHandrail:['RightHandrail1','RightHandrail2','RightHandrail3','RightHandrail4','RightHandrail5'],
+    RightHandrail1:['RightHandrail11','RightHandrail12','RightHandrail13','RightHandrail14','RightHandrail15','RightHandrail16','RightHandrail17','RightHandrail18'],
+    LeftWall:['LeftWall1','LeftWall2','LeftWall3','LeftWall4','LeftWall5','LeftWall6'],
+    RightWall:['RightWall1','RightWall2','RightWall3','RightWall4','RightWall5','RightWall6','_0601K_50cr_4_Body_0_color_Legacy_135'],
+    Bumper:['LeftBumper','RightBumper','BackBumperl'],
+};
+export const Floor = 'Floor';
+export const DisplayHorisontal = 'DisplayHorisontal';
+export const DisplayVertical = 'DisplayVertical';
+export const BackHandrail = 'BackHandrail';
+export const Mirror = 'Mirror';
+export const LeftBumper = 'LeftBumper';
+export const RightBumper = 'RightBumper';
+export const BackBumper = 'BackBumperl';
+export const Threshold = 'Threshold';
+export const Threshold1 = 'Threshold1';
